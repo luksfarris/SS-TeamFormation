9ee8ffc80827c3a16a06d2c4b97d03e796f3cce8
Jowjow <araujowjow@gmail.com>
2014-12-05 23:22:47 -0200

    Update Instancia.java

--- TeamFormationScatterSearch/src/main/Instancia.java
+++ TeamFormationScatterSearch/src/main/Instancia.java
@@ -1,6 +1,11 @@
 package main;
 
 import java.util.List;
+import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 /**
  * @author Lucas Farris
@@ -23,6 +28,7 @@ public class Instancia {
 	/** Budget maximo. */
 	double budget;
 	
+
 	public Instancia() {
 		lerDados();
 	}
@@ -31,9 +37,51 @@ public class Instancia {
 	 * Le os arquivos de herois e do grafo, e cria as estruturas de dados.
 	 */
 	public void lerDados() {
-		// TODO: ler os csv's. CSV nada mais eh que um txt organizado, entao da pra ler
-		// normal. Essa função precisa peencher as variaveis herois, viloes e grafo!!
-		//Vou fazer essa parte aqui - André
+		BufferedReader reader = null;
+		File file = new File("assetsmarvel_character-victorfc.csv");
+		String path = file.getAbsolutePath();
+		String dado = "";
+		String splitBy = ";";
+		
+		try {
+			FileReader fr = new FileReader("C:\\Jowjow\\pathto\\marvel_character-victorfc.csv");
+			reader = new BufferedReader(fr);
+			dado = reader.readLine();
+			while ((dado = reader.readLine()) != null) {
+	 
+				String[] dados = dado.split(splitBy); 
+				
+				Personagem novo = new Personagem();
+				novo.setId(Integer.parseInt(dados[0]));
+				novo.setPowerGrid(1, Integer.parseInt(dados[3]));
+				novo.setPowerGrid(2, Integer.parseInt(dados[4]));
+				novo.setPowerGrid(3, Integer.parseInt(dados[5]));
+				novo.setPowerGrid(4, Integer.parseInt(dados[6]));
+				novo.setPowerGrid(5, Integer.parseInt(dados[7]));
+				novo.setPowerGrid(6, Integer.parseInt(dados[8]));
+				novo.setPopularidade(Integer.parseInt(dados[9]));
 
+				if(dados[2].equals("Hero")){
+					novo.setHeroi(true);
+				}
+				else
+					novo.setHeroi(false);
+
+			}
+		}
+	  catch (FileNotFoundException e) {
+		e.printStackTrace();
+	  } catch (IOException e) {
+		  e.printStackTrace();
+	  } finally {
+		  if (reader != null) {
+				try {
+					reader.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+	  }
 	}
+
 }
