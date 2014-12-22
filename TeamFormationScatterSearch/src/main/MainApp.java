31d4cd6b022922033465b7acb6e2dd0bdbf4d112
Lucas <luksfarris@gmail.com>
2014-12-22 02:01:07 -0200

    Update MainApp.java

--- TeamFormationScatterSearch/src/main/MainApp.java
+++ TeamFormationScatterSearch/src/main/MainApp.java
@@ -44,14 +44,14 @@ public class MainApp {
 	public static void main(String[] args) {
 		
 		Instancia instancia = new Instancia();
-		instancia.budget = Double.MAX_VALUE;
-		int viloes[] = getViloes(2);
+		int viloes[] = getViloes(9);
+		instancia.budget = 763*viloes.length;
 		instancia.minHerois = 1;
 		instancia.maxHerois = viloes.length + 1;
 		instancia.viloes = viloes;
 		
 		ScatterSearch search = new ScatterSearch(instancia, 20);
-		Solucao solucao = search.busca(2000);
+		Solucao solucao = search.busca(3000);
 		if (solucao != null) {
 			System.out.println("Melhor solução encontrada tem valor " + solucao.avalia(instancia));
 			System.out.println("S = " + solucao);
